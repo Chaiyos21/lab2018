aa639cdc1ae0c856efbb90dd8f15d4b4e9af391d
Chaiyos21 <chaiyos.p@rmutsvmail.com>
2025-01-15 08:23:19 +0700

    แกไขไฟล์ ครั้งที่ 1

--- contact.php
+++ contact.php
@@ -17,6 +17,7 @@
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
+                <li><a href="contactdetal.php">ติดต่อสอบถาม</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
